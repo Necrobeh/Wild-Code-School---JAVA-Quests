0cf7fc1de90ac9d79ab85a9ae8b4cc5c8e262bf3
mickaël blanchet <mickael.blanchet5555@gmail.com>
2022-10-25 15:03:59 +0200

    Wild Code School - Java Quest N°08

--- Eagle.java
+++ Eagle.java
@@ -1,4 +1,4 @@
-public class Eagle extends Bird {
+public class Eagle extends Bird implements Fly{
 
     private boolean flying;
     private int altitude;
@@ -21,4 +21,57 @@ public class Eagle extends Bird {
     public String sing() {
         return "Screech!";
     }
+
+    @Override
+    public void takeOff(){
+        if(this.getAltitude() == 0 && this.isFlying() == false){
+            this.flying = true;
+            System.out.println(this.getName() + "has taken off !");
+        }else{
+            System.out.println(this.getName() + "is already flying");
+        }
+    }
+
+    @Override
+    public int ascend(int meters){
+        if(this.isFlying() == true){
+            this.altitude = Math.min(this.getAltitude() + meters, 6096);
+            System.out.println(this.getName() + " is now flying at " + this.getAltitude() + " meters");
+        }else{
+            System.out.println(this.getName() + " is not flying yet !");
+        }
+        return this.altitude;
+    }
+
+    @Override
+    public int descend(int meters){
+        if(this.isFlying() == true){
+            this.altitude = Math.max(this.getAltitude() - meters, 0);
+            System.out.println(this.getName() + " is now flying at " + this.getAltitude() + " meters");
+        }else{
+            System.out.println(this.getName() + " is not flying yet !");
+        }
+        return this.altitude;
+    }
+
+    @Override
+    public void land(){
+        if(this.isFlying() == true && this.getAltitude() == 0){
+            this.flying = false;
+            System.out.println(this.getName() + " has landed !");
+        }else if(this.isFlying() == false){
+            System.out.println(this.getName() + " is not flying yet !");
+        }else{
+            System.out.println(this.getName() + " is still flying !");
+        }
+    }
+
+    @Override
+    public void glide(){
+        if(this.isFlying() == true){
+            System.out.println(this.getName() + " is gliding nicely !");
+        }
+    }
+
+
 }
